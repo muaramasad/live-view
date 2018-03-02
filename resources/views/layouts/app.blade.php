fe68992d09323fc147b223e69476a417e32f4733
JAKARTA\muharram.asad <asadmuharram@gmail.com>
2018-03-02 16:06:55 +0700

    remove logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
 			<nav class="navbar" role="navigation" aria-label="main navigation">
 				<div class="navbar-brand">
 					<a class="navbar-item" href="{{route('homepage')}}">
-						<img src="{{ asset('images/japfa_logo.png')}}" alt="Japfa Monitoring System" width="auto" height="28">
+						{{-- <img src="{{ asset('images/japfa_logo.png')}}" alt="Japfa Monitoring System" width="auto" height="28"> --}}
 					</a>
 					<button class="button navbar-burger">
 					<span></span>
