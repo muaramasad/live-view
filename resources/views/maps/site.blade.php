9a224a930bf922271f810c59ac38bf5a4eddc888
JAKARTA\muharram.asad <asadmuharram@gmail.com>
2018-03-14 14:26:28 +0700

    update cctv path

--- resources/views/maps/site.blade.php
+++ resources/views/maps/site.blade.php
@@ -90,18 +90,18 @@
 			var date = moment(new Date());
 			date = date.subtract(3, 'minutes');
     		newYearCountdown = setInterval(function(){
-    		if (counter === 601) {
+    		if (counter === 241) {
 				counter = 1;
 				date = date.add(1, 'minutes');
 			}
         	var newImg = $('#test');
         	var nameCam = $('#camName');
-        	var filename = 'http://{{$site->url_1}}/'+path+'/ip-'+date.format("DDMMYYYY__HHmm")+'-'+counter+'.jpeg';
+        	var filename = '/video/'+path+'/ip-'+counter+'.jpeg';
         	newImg.attr("src", filename);
         	nameCam.text(camName);
         	console.log(filename);
         	counter++;
-			}, 1000/10);
+			}, 1000/2);
 		}
 		$(document).on('click', '.modal-close', function() {
 			 $(".modal").removeClass("is-active");
